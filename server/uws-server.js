8be2e89b894267a42b2296eefaa3e0aa999e44c3
william-taylor <wi11berto@yahoo.co.uk>
2017-07-13 21:38:37 +0100

    adding redirect for server default path

--- server/uws-server.js
+++ server/uws-server.js
@@ -13,7 +13,11 @@ app.post('/getNews', (req, res) => {
   fs.readFile("json/news.json", 'utf8', function(err, data) {
     res.json(JSON.parse(data));
   })
-})
+});
+
+app.get('/', (req, res) => {
+  res.redirect('https://play.google.com/store/apps/details?hl=iw&id=com.uws.campus_app');
+});
 
 const server = app.listen(3005, () => {
   console.log(`UWS Server Online ${JSON.stringify(server.address())}`);
